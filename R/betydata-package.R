0a1b59b9caa86399f8604c358fc9f8de515f009d
divne7022 <www.divinehome12@gmail.com>
2026-02-11 14:26:53 -0500

    add dataset doc

---
+++ R/betydata-package.R
@@ -0,0 +1,8 @@
+#' betydata: Public Data from the BETYdb Database
+#'
+#' Offline access to plant trait and yield data from BETYdb.
+#' Load datasets with `data(traitsview)`, `data(species)`, etc.
+#'
+#' @seealso [traitsview], [species], [sites], [variables], [pfts], [priors]
+#' @keywords internal
+"_PACKAGE"
